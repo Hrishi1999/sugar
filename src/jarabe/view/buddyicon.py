Hrishi1999/sugar

commit 8ddec3efd9d5fc3c877914066d1691687da52d4c

Update buddyicon.py

src/jarabe/view/buddyicon.py
@@ -60,9 +60,10 @@ class BuddyIcon(CanvasIcon):
 
         self.__datastore_query()
         self.__tamagotchi_thread()
+        GObject.timeout_add(1000, self.__tamagotchi_thread)
+        GObject.timeout_add(100000, self.__datastore_query)
 
     def __tamagotchi_thread(self):
-        GObject.timeout_add(1000, self.__tamagotchi_thread)
 
         user_type = None
         disk_space_type = None
@@ -88,7 +89,6 @@ class BuddyIcon(CanvasIcon):
         self.set_icon_name(self.icon_dict[user_type][disk_space_type])
 
     def __datastore_query(self):
-        GObject.timeout_add(100000, self.__datastore_query)
         test, entries = datastore.find({})
         self.journal_entries = entries
 

commit 69c90e2ebd967ffb389db7dced3e3a43d6f37970

Undo #8ddec3e
Caused the widget to not update

src/jarabe/view/buddyicon.py
@@ -60,10 +60,9 @@ class BuddyIcon(CanvasIcon):
 
         self.__datastore_query()
         self.__tamagotchi_thread()
-        GObject.timeout_add(1000, self.__tamagotchi_thread)
-        GObject.timeout_add(100000, self.__datastore_query)
 
     def __tamagotchi_thread(self):
+        GObject.timeout_add(1000, self.__tamagotchi_thread)
 
         user_type = None
         disk_space_type = None
@@ -89,6 +88,7 @@ class BuddyIcon(CanvasIcon):
         self.set_icon_name(self.icon_dict[user_type][disk_space_type])
 
     def __datastore_query(self):
+        GObject.timeout_add(100000, self.__datastore_query)
         test, entries = datastore.find({})
         self.journal_entries = entries
 